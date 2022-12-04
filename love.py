627d4f2e2977bbc1ca4edd40716b1640c35b35c3
MAHIN-15 <119294493+MAHIN-15@users.noreply.github.com>
2022-12-04 23:53:41 +0600

    Add files via upload

--- love.py
+++ love.py
@@ -1,16 +1,72 @@
+import os
+import time
+import sys
+
+os.system("clear")
+
+print('''\033[91m
+  __  __          _    _ _____ _   _  
+ |  \/  |   /\   | |  | |_   _| \ | | 
+ | \  / |  /  \  | |__| | | | |  \| | 
+ | |\/| | / /\ \ |  __  | | | | . ` | 
+ | |  | |/ ____ \| |  | |_| |_| |\  | 
+ |_|  |_/_/    \_\_|  |_|_____|_| \_| 
+------------------------------------------------------------------------------
+[+] Tool Name  :  proposed 
+[+] Author         :   MAHIN90
+[+] Developer   :  MD MAHMUD MAHIN 
+[+] Contact      :   01704673950
+-------------------------------------------------------------------------------
+ os.system('xdg-open https://www.facebook.com/mgggjuc')
+
+''')
 def slowprint(s):
     for c in s + '\n' :
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(8. / 100)
-print("\033[95m+-------------------------------------------------+")
-slowprint('''\033[95m|       _____   _      ______      ________  __     ______  _    _  
- |_   _| | |    / __ \ \    / /  ____| \ \   / / __ \| |  | | 
-   | |   | |   | |  | \ \  / /| |__     \ \_/ / |  | | |  | | 
-   | |   | |   | |  | |\ \/ / |  __|     \   /| |  | | |  | | 
-  _| |_  | |___| |__| | \  /  | |____     | | | |__| | |__| | 
- |_____| |______\____/   \/   |______|    |_|  \____/ \____/  
-                                                              
+        time.sleep(5. / 100)
+
+slowprint (''' \033[95m
++--------------------------------------+
+| DEAR ? YOU ARE MY LOVE YOU ARE MY HEART YOU ATE MY LIFE  |
++--------------------------------------+
+| I DON'T KNOW HOW CAN I SAY YOU |
++--------------------------------------+''')
+
+slowprint(''' \033[93m
+I love that you are you and the way you make me feel. 
+I love that you are kind, affectionate, thoughtful, so so funny, silly, and loving. 
+I love that you are hard working, have dreams, that you are interesting, that you look me in the eyes when we talk, and that you hug me longer than most people do. ''')                   
+
+    slowprint(''' \033[95m
+ _____                       
+ |_   _|                      
+   | |                        
+   | |                        
+  _| |_                       
+ |_____| ______      ________ 
+ | |    / __ \ \    / /  ____|
+ | |   | |  | \ \  / /| |__   
+ | |   | |  | |\ \/ / |  __|  
+ | |___| |__| | \  /  | |____ 
+ |______\____/  _\/  _|______|
+ \ \   / / __ \| |  | |       
+  \ \_/ / |  | | |  | |       
+   \   /| |  | | |  | |       
+    | | | |__| | |__| |       
+    |_|  \____/ \____/ ''')      
+                              
+                              
+   
+        
+                  
+                     
+
+   
+                              
+                              
+
+   
                                                               
         
         
